69b4e30315c12d8764c67b78c245d8150a909d78
zzsjhp <zzsj8897@gmail.com>
2025-05-17 08:42:24 +0800

    fix errors in subwordProp.lean

--- HeckeAlgebra/SubwordProp.lean
+++ HeckeAlgebra/SubwordProp.lean
@@ -28,15 +28,15 @@ lemma subword_of_lt_adj (veq : v = π ω) (h : lt_adj cs u v) :
     obtain ⟨ ⟨t, vut⟩, llt⟩ := h
     have vtu : v * t = u := by rw [←IsReflection.inv vut.1, vut.2]; group
     rw [←vtu, veq] at llt
-    obtain ⟨ l, ⟨h1, ⟨i, hi⟩ ⟩ ⟩ := StrongExchange cs vut.1 llt
-    by_cases lred : IsReduced cs l
-    · refine ⟨l, ⟨⟨lred, by rw [h1, ←veq, vtu]⟩, ?_ ⟩⟩
-      rw [hi]; exact eraseIdx_sublist ω i.1
-    · let ll      := choose <| DeletionProperty cs lred
-      let ll_spec := choose_spec <| DeletionProperty cs lred
+    obtain ⟨ k, hk ⟩ := strongExchangeProperty' cs ω ⟨t, vut.1⟩ ⟨vut.1, llt⟩
+    by_cases lred : IsReduced cs (ω.eraseIdx k)
+    · use ω.eraseIdx k
+      exact ⟨ ⟨lred, by simp [←vtu, veq, hk]⟩, eraseIdx_sublist ω k⟩
+    · let ll_spec := choose_spec <| DeletionProperty cs lred
+      set ll      := choose <| DeletionProperty cs lred
       refine  ⟨ll, ⟨?_ , ?_ ⟩⟩
-      · exact ⟨ll_spec.2.2, by rw [←vtu, ll_spec.2.1, h1, ←veq]⟩
-      · exact ll_spec.1.trans (by rw [hi]; exact eraseIdx_sublist ω i.1)
+      · exact ⟨ll_spec.2.2, by rw [←vtu, ll_spec.2.1, veq, hk]⟩
+      · exact ll_spec.1.trans (eraseIdx_sublist ω k)
 
 /-- If u < v, then exists reduced word of u is subword of reduced word of v. -/
 lemma subword_of_lt (veq : v = π ω) (h : u < v)  : ∃ ω' : List B,
